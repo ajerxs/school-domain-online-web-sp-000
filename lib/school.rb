21748322be1b51424ed9f59611895730d058e94b
Andrew Eames <ajerxs@gmail.com>
2020-11-23 01:42:39 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,15 +7,15 @@ class School
   end
 
   def roster
-    @roster = {}
+    roster = {}
   end
 
   def add_student(student, grade)
-    if @roster[grade]
-      @roster[grade] << student
+    if roster[grade]
+      roster[grade] << student
     else
-      @roster[grade] = []
-      @roster[grade] << student
+      roster[grade] = []
+      roster[grade] << student
     end
   end
 
